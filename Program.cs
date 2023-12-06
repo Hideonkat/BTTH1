994241277412ceb5b8c707959730edc850685667
Hideonkat <danghung0711dh@gmail.com>
2023-12-06 21:36:32 +0700

    BTTH1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Systemtime
+{
+ class Program
+{
+    private static void Main(string[] args)
+    {
+        DateTime localTime = DateTime.Now;
+        Console.WriteLine("Thoi gian tuong lai la: " + localTime );
+    }
+}
+
+    
+
+}
